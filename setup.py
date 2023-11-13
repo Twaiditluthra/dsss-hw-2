23bc25957e465a8c9ea540ec9d8e867e451bd7f4
Twaiditluthra <twaiditluthra@gmail.com>
2023-11-13 23:43:44 +0100

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="math_quiz",
+    version="0.1.0",
+    description="solving_maths_problem_to_score_points",
+    author="N",
+    author_email="twaiditluthra@gmail.com",
+    url="https://github.com/Twaiditluthra/dsss-hw-2",
+    packages=["math_quiz"],
+    install_requires=[],
+)
